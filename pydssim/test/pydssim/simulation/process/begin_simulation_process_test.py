18784e88cbfa051125dfe6e4c0edf06591a77ff3
lglmoura <lglmoura@d73c2cbc-9777-11de-87fe-ddda47adee5a>
2009-10-27 01:52:55 +0000

    test

---
+++ pydssim/test/pydssim/simulation/process/begin_simulation_process_test.py
@@ -0,0 +1,21 @@
+"""
+@author: Luiz Gustavo 
+@organization: Federal University of Rio de Janeiro
+@contact: lglmoura@cos.ufrj.br 
+@since: 6/07/2009
+"""
+
+from pydssim.simulation.process.begin_simulation_process import BeginSimulationProcess
+from pymockobject.events import ReturnValue
+import pymockobject
+
+import unittest
+
+class BeginSimulationProcessTest(unittest.TestCase):
+    
+    def testCreateClassInstance(self):
+        self.assertTrue(BeginSimulationProcess())
+        handler = BeginSimulationProcess()
+        self.assertEquals("BEGIN_SIMULATION", handler.getIdentifier())
+       
+   
