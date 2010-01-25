92483541908fa9c26884719e29da5d02dee06e51
lglmoura <lglmoura@d73c2cbc-9777-11de-87fe-ddda47adee5a>
2010-01-25 17:37:33 +0000

    dispatcher message

--- pydssim/src/pydssim/network/dispatcher/abstract_message_handler.py
+++ pydssim/src/pydssim/network/dispatcher/abstract_message_handler.py
@@ -1,35 +1,42 @@
-from pydssim.util.protected import Protected
-from pydssim.util.decorator.require import require
-from pydssim.util.decorator.public import public
-from pydssim.util.decorator.return_type import return_type
-from pydssim.network.message.i_message import IMessage
+from pydssim.util.logger import Logger
 
-class AbstractMessageHandler(Object):
+class AbstractMessageHandler():
+    
+    PEERNAME   = "NAME"   # request a peer's canonical id
+    LISTPEERS  = "LIST"
+    INSERTPEER = "JOIN"
+    QUERY      = "QUER"
+    QRESPONSE  = "RESP"
+    FILEGET    = "FGET"
+    PEERQUIT   = "QUIT"
+    SUPERPEER  = "SUPE"
+    REPLY      = "REPL"
+    ERROR      = "ERRO"
+    PEERFULL   = "PEFU"
+    PEEREXIT   = "EXIT" 
     
     def __init__(self):
         raise NotImplementedError()
     
-    def initialize(self, messageName, peer):
+    def initialize(self,  peer, messageName, canID):
         self.__messageName = messageName
         self.__peer = peer
-        self.__message = None
+        self.__canID = canID
+        Logger().resgiterLoggingInfo("Message => Create Message Handler %s can %s"%(self.__messageName,self.__canID))
+                                     
     
-    @public
+    
     def getMessageName(self):
         return self.__messageName
     
-    @public
+    
     def getPeer(self):
         return self.__peer
     
-    @public
-    def getMessage(self):
-        return self.__message
+  
+    def getCanID(self):
+        return self.__canID
     
-    @public
-    def handleMessage(self, message):
-        self.__message = message
-        return self.executeHandler()
         
-    def executeHandler(self):
+    def executeHandler(self,peerConn,data):
         raise NotImplementedError()
--- pydssim/src/pydssim/network/dispatcher/message_dispatcher.py
+++ pydssim/src/pydssim/network/dispatcher/message_dispatcher.py
@@ -1,9 +1,8 @@
-from pydssim.util.protected import Protected
-from pydssim.util.decorator.public import public
+
 from threading import Thread
 from pydssim.util.logger import Logger
 
-class MessageDispatcher(Protected):
+class MessageDispatcher():
     
     def __init__(self, peer):
         self.initialize(peer)
@@ -11,20 +10,29 @@ class MessageDispatcher(Protected):
     def initialize(self, peer):
         self.__peer = peer
         self.__messageHandlers = {}
-        Logger().resgiterLoggingInfo("Initialize Repository pid  %s of peer %s "%(self.__class__.__name__,self.__peer.getURN()))
+        Logger().resgiterLoggingInfo("Initialize MessageHamdles pid  %s of peer %s "%(self.__class__.__name__,self.__peer.getURN()))
     
-    @public
+    def getMessageHandlers(self):
+        return self.__messageHandlers
+    
     def registerMessageHandler(self, messageHandler):
-        if self.__messageHandlers.has_key(messageHandler.getMessageName()):
+        if self.__messageHandlers.has_key(messageHandler.getCanID()):
             raise StandardError()
-        self.__messageHandlers[messageHandler.getMessageName()] = messageHandler
-        return self.__messageHandlers[messageHandler.getMessageName()]
+        self.__messageHandlers[messageHandler.getCanID()] = messageHandler
+        return self.__messageHandlers[messageHandler.getCanID()]
     
-    @public
+    
     def countMessageHandlers(self):
         return len(self.__messageHandlers)
     
-    @public
+    def hasTypeMessage(self,msgType):
+        
+        if self.__messageHandlers.has_key(msgType):
+            return True
+        else:
+            return False    
+        
+        
     def unregisterMessageHandler(self, messageName):
         if not self.__messageHandlers.has_key(messageName):
             raise StandardError()
@@ -32,21 +40,18 @@ class MessageDispatcher(Protected):
         del self.__messageHandlers[messageName]
         return messageHandler
     
-    @public
-    def handleMessage(self, message):
-        if not self.__messageHandlers.has_key(message.getName()):
+    def executeHandleMessage(self,messageName,peerConn,data):
+        if not self.__messageHandlers.has_key(messageName):
             raise StandardError()
-        handler = self.__messageHandlers[message.getName()]
-        handler.handleMessage(message)
+        handler = self.__messageHandlers[messageName]
+        handler.executeHandler(peerConn,data)
 #       
-        return message
+        return handler
     
-    class MessageHandlingThread(Thread):
+    def handleMessage(self, messageName):
+        if not self.__messageHandlers.has_key(messageName):
+            raise StandardError()
+        return self.__messageHandlers[messageName]
         
-        def __init__(self, handler, message):
-            Thread.__init__(self)
-            self.__handler = handler
-            self.__message = message
-            
-        def run(self):
-            self.__handler.handleMessage(self.__message)
+    
+    
