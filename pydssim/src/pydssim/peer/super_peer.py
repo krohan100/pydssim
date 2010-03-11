5a909cc3c9339b3fce8053f34e162a63ff1a8e1d
lglmoura <lglmoura@d73c2cbc-9777-11de-87fe-ddda47adee5a>
2010-03-11 21:43:42 +0000

    superpeer

---
+++ pydssim/src/pydssim/peer/super_peer.py
@@ -0,0 +1,28 @@
+"""
+Defines the module with the implementation AbstractPeer class.
+
+@author: Luiz Gustavo 
+@organization: Federal University of Rio de Janeiro
+@contact: lglmoura@cos.ufrj.br 
+@since: 28/10/2009
+"""
+
+from pydssim.peer.abstract_peer import AbstractPeer
+from pydssim.util.decorator.public import public,createURN
+
+class SuperPeer(AbstractPeer):
+    """
+    Implements the basic functions of a peer.
+    @author: Luiz Gustavo
+    @organization: Federal University of Rio de Janeiro
+    @contact: lglmoura@cos.ufrj.br 
+    @since: 22/08/2009
+    """
+
+    def __init__(self,network, urn=createURN("peer"),serverPort=4000):
+        
+        AbstractPeer.initialize(self, network,urn,serverPort,network.getMaxNeighbor(), peerType = AbstractPeer.SUPER)
+    
+    
+
+        
